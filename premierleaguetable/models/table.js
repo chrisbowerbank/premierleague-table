f2eeede3146c8208bd820da457bf33555fedb004
cb <misterbowerbank@gmail.com>
2017-04-23 09:32:28 -0400

    initial commit

---
+++ premierleaguetable/models/table.js
@@ -0,0 +1,14 @@
+// var mongoose = require('mongoose')
+// var Schema = mongoose.Schema
+//
+// var rowSchema = new Schema({
+//   position: Number,
+//   teamName: String,
+//   crestURI: String,
+//   playedGames: String,
+//   points: Number
+// })
+//
+// var Row = mongoose.model( 'index', rowSchema )
+//
+// module.exports = Row
